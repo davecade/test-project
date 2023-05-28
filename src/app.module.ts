b2081cd959cbda7c324a307ffdc2573ec4bc2c2b
Dave <dave.cadelina@outlook.com>
2023-05-29 00:27:28 +1000

    Add fallback controller for client-side routes finally works 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path'
+import { ServeStaticModule } from '@nestjs/serve-static'
+//import { FallbackController } from './fallback.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/build'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
