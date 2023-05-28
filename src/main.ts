fbe908dd47d297abe44f4fa2106afeeb91f7d7a3
Dave <dave.cadelina@outlook.com>
2023-05-28 23:12:48 +1000

    updated again 4

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { join } from 'path'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
+  app.use(express.static(join(__dirname, '..', 'client', 'build'))); // Serve the React build files
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
