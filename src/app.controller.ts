301c5f2a1dbb6ff5c0ecd45ee1061f7a628af2f9
Dave <dave.cadelina@outlook.com>
2023-05-29 00:45:23 +1000

    removed comment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,9 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';;
 import { AppService } from './app.service';
-import { Request, Response } from 'express';
-import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  // @Get('/')
-  // root(@Req() req: Request, @Res() res: Response): void {
-  //   res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
-  // }
 
   @Get('/hello')
   getHello(): string {
