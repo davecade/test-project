10e5460962bd00dcce4779eb3be14b6a56e10331
Dave <dave.cadelina@outlook.com>
2023-06-02 02:34:42 +1000

    added .env in nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { join } from 'path'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import { ConfigModule } from '@nestjs/config';
+
+// to access .env file in nest, you need to download:
+// npm install @nestjs/config
+// then import ConfigModule and use it with ConfigModule.forRoot() below
 
 @Module({
   imports: [
@@ -10,6 +15,7 @@ import { ServeStaticModule } from '@nestjs/serve-static'
     [ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/build'),
     })] : []),
+    ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
